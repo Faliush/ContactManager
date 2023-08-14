fba49b2458d144ee393480c98029c61fe7caf66d
nickfaliush <faliushdev@gmail.com>
2023-08-14 17:00:41 +0300

    Create logout method in IdentityServer

--- IdentityServer/Controllers/AccountController.cs
+++ IdentityServer/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using IdentityServer.Models;
+﻿using IdentityServer.Data;
+using IdentityServer.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityServer.Controllers
@@ -6,6 +9,20 @@ namespace IdentityServer.Controllers
     [Route("[controller]")]
     public class AccountController : Controller
     {
+        private SignInManager<ApplicationUser> _signInManager;
+        private UserManager<ApplicationUser> _userManager;  
+        private IIdentityServerInteractionService _interaction;
+
+        public AccountController(
+            SignInManager<ApplicationUser> signInManager, 
+            UserManager<ApplicationUser> userManager, 
+            IIdentityServerInteractionService interaction)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _interaction = interaction;
+        }
+
         [HttpGet("[action]")]
         public IActionResult Login()
         {
@@ -28,6 +45,18 @@ namespace IdentityServer.Controllers
         public IActionResult Register(RegisterViewModel registerViewModel)
         {
             return View(registerViewModel);
+        }
+
+        [Route("[action]")]
+        public async Task<IActionResult> Logout(string logoutId)
+        {
+            await _signInManager.SignOutAsync();
+            var logoutResult = await _interaction.GetLogoutContextAsync(logoutId);
+
+            if (string.IsNullOrEmpty(logoutResult.PostLogoutRedirectUri))
+                return RedirectToAction("Login", "Account");
+
+            return Redirect(logoutResult.PostLogoutRedirectUri);
         }
     }
 }
