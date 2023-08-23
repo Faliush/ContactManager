2518df81b7e88b9c9c3026658af2b186cd7358e4
nickfaliush <faliushdev@gmail.com>
2023-08-23 13:00:35 +0300

    add Repositories and UnitOfWork to the infrastructure

---
+++ Faliush.ContactManager.Infrastructure/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿namespace Faliush.ContactManager.Infrastructure.UnitOfWork;
+
+public interface IUnitOfWork<out TContext> : IDisposable
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    TContext DbContext { get; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <returns></returns>
+    IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    int SaveChanges();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    Task<int> SaveChangesAsync();
+
+    SaveChangesResult LastSaveChangeResult { get; }
+}
