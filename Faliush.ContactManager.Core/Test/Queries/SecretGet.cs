769985df6100c15318072c02858213e253df21f2
nickfaliush <faliushdev@gmail.com>
2023-08-23 18:31:49 +0300

    Rewrite code from controllers to minimal api

---
+++ Faliush.ContactManager.Core/Test/Queries/SecretGet.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Faliush.ContactManager.Core.Test.Queries;
+
+public record SecretGetRequest() : IRequest<string>;
+
+public class SecretGetRequestHandler : IRequestHandler<SecretGetRequest, string>
+{
+    public async Task<string> Handle(SecretGetRequest request, CancellationToken cancellationToken)
+    {
+        return "This is secret";
+    }
+}
