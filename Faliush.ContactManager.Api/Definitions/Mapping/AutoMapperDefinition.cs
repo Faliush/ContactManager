d8ff19f92e52f30a7fc17fdf22bdc71fd86593f2
nickfaliush <faliushdev@gmail.com>
2023-08-23 20:24:15 +0300

    Add AutoMapper to the project and create definition for it

---
+++ Faliush.ContactManager.Api/Definitions/Mapping/AutoMapperDefinition.cs
@@ -0,0 +1,21 @@
+﻿using Faliush.ContactManager.Api.Definitions.Base;
+
+namespace Faliush.ContactManager.Api.Definitions.Mapping;
+
+public class AutoMapperDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(typeof(Program));
+    }
+
+    public override void ConfigureApplication(WebApplication app)
+    {
+        var mapper = app.Services.GetRequiredService<AutoMapper.IConfigurationProvider>();
+        
+        if (app.Environment.IsDevelopment())
+            mapper.AssertConfigurationIsValid();// validate Mapper Configuration
+        else
+            mapper.CompileMappings();
+    }
+}
