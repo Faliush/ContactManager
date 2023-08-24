bd9ddfc384e9264c0ff09bacc5c891629aa59c23
nickfaliush <faliushdev@gmail.com>
2023-08-24 21:10:54 +0300

    Add all endpoints

--- Faliush.ContactManager.Api/Endpoints/CountryEndpoints/CountryEndpoints.cs
+++ Faliush.ContactManager.Api/Endpoints/CountryEndpoints/CountryEndpoints.cs
@@ -11,12 +11,23 @@ public class CountryEndpoints : AppDefinition
     {
         app.MapGet("api/country/get-all", GetAllCountries);
         app.MapPost("api/country/create", CreateCountry);
+        app.MapDelete("api/country/delete/{id:guid}", DeleteCountry);
+        app.MapGet("api/country/update/{id:guid}", GetForUpdateCountry);
+        app.MapPut("api/country/update", PutAfterUpdateCountry);
     }
-
 
     private async Task<List<CountryViewModel>> GetAllCountries(IMediator mediator, HttpContext context) =>
         await mediator.Send(new CountryGetAllRequest(), context.RequestAborted);
 
     private async Task<CountryViewModel> CreateCountry(IMediator mediator, CountryCreateViewModel viewModel, HttpContext context) =>
         await mediator.Send(new CountryCreateRequest(viewModel), context.RequestAborted);
+
+    private async Task<Guid> DeleteCountry(Guid id, IMediator mediator, HttpContext context) =>
+        await mediator.Send(new CountryDeleteRequest(id), context.RequestAborted);
+
+    private async Task<CountryUpdateViewModel> GetForUpdateCountry(Guid id, IMediator mediator, HttpContext context) =>
+        await mediator.Send(new CountryGetForUpdateRequest(id), context.RequestAborted);
+
+    private async Task<CountryViewModel> PutAfterUpdateCountry(CountryUpdateViewModel viewModel, IMediator mediator, HttpContext context) =>
+        await mediator.Send(new CountryUpdateRequest(viewModel), context.RequestAborted);
 }
