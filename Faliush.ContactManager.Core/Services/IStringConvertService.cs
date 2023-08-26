eff03a664a840eeed2ca093e054e56f273308b27
nickfaliush <faliushdev@gmail.com>
2023-08-26 21:09:21 +0300

    Add service for getting years of people and converting string to enum

---
+++ Faliush.ContactManager.Core/Services/IStringConvertService.cs
@@ -0,0 +1,6 @@
+﻿namespace Faliush.ContactManager.Core.Services;
+
+public interface IStringConvertService
+{
+    public T ConvertToEnum<T>(string value) where T : struct;
+}
---
+++ Faliush.ContactManager.Core/Services/StringConvertService.cs
@@ -0,0 +1,17 @@
+﻿using Faliush.ContactManager.Core.Exceptions;
+using Faliush.ContactManager.Models.Base;
+using System.Net.Http.Headers;
+
+namespace Faliush.ContactManager.Core.Services;
+
+public class StringConvertService : IStringConvertService
+{
+    public T ConvertToEnum<T>(string value) where T : struct
+    {
+        if(Enum.TryParse<T>(value, out T result))
+            return result;
+
+        throw new ContactManagerInvalidOperationException("cannot convert string to enum");
+       
+    }
+}
