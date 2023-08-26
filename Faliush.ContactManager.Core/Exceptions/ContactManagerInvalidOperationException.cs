2ce1287aaa6d74022a6e49050efa25890993a895
nickfaliush <faliushdev@gmail.com>
2023-08-26 21:13:01 +0300

    create two custom service exceptions

---
+++ Faliush.ContactManager.Core/Exceptions/ContactManagerInvalidOperationException.cs
@@ -0,0 +1,9 @@
+﻿namespace Faliush.ContactManager.Core.Exceptions;
+public class ContactManagerInvalidOperationException : Exception
+{
+    public ContactManagerInvalidOperationException() : base("something went wrong") { }
+
+    public ContactManagerInvalidOperationException(string? message) : base(message) { }
+
+    public ContactManagerInvalidOperationException(string? message, Exception? exception) : base(message, exception) { }
+}
