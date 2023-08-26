b48ec8fcbe2a1f33a1efdb45e7a1de0e3640641c
nickfaliush <faliushdev@gmail.com>
2023-08-26 12:57:50 +0300

    add prhone number to person entity

---
+++ Faliush.ContactManager.Infrastructure/Migrations/20230826095619_AddPhoneNumberToPersonEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Faliush.ContactManager.Infrastructure.Migrations
+{
+    public partial class AddPhoneNumberToPersonEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "People");
+        }
+    }
+}
