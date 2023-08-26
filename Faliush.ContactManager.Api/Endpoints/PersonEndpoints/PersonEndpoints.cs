50f169cde31dadf76ea30caee690cb2285154050
nickfaliush <faliushdev@gmail.com>
2023-08-26 12:33:56 +0300

    Completed Person enpoints

--- Faliush.ContactManager.Api/Endpoints/PersonEndpoints/PersonEndpoints.cs
+++ Faliush.ContactManager.Api/Endpoints/PersonEndpoints/PersonEndpoints.cs
@@ -1,4 +1,6 @@
 ﻿using Faliush.ContactManager.Api.Definitions.Base;
+using Faliush.ContactManager.Core.Enums;
+using MediatR;
 
 namespace Faliush.ContactManager.Api.Endpoints.PersonEndpoints;
 
@@ -9,9 +11,40 @@ public class PersonEndpoints : AppDefinition
         app.MapGet("api/people", GetAllPeople);
         app.MapGet("api/people/filtered", GetFilteredPeople);
         app.MapGet("api/people/{id:guid}", GetPersonById);
+        app.MapGet("api/people/create", GetForCreate);
         app.MapPost("api/people", CreatePerson);
         app.MapDelete("api/people", DeletePerson);
         app.MapGet("api/people/update/{id:guid}", GetPersonForUpdate);
         app.MapPut("api/people", PutAfterUpdatePerson);
     }
+
+    private async Task<List<PeopleViewModel>> GetAllPeople(IMediator mediator, HttpContext context) =>
+        await mediator.Send(new PersonGetAllRequest(), context.RequestAborted);
+
+    private async Task<List<PeopleViewModel>> GetFilteredPeople(
+        IMediator mediator,
+        HttpContext context,
+        string? searchBy,
+        string? searchString,
+        string? sortBy,
+        SortOptions sortOrder = SortOptions.Ascending) =>
+            await mediator.Send(new PersonGetFilteredRequest(searchBy, searchString, sortBy, sortOrder), context.RequestAborted);
+
+    private async Task<PersonViewModel> GetPersonById(Guid id, IMediator mediator, HttpContext context) =>
+        await mediator.Send(new PersonGetByIdRequest(), context.RequestAborted);
+
+    private async Task<PersonCreateViewModel> GetForCreate(IMediator mediator, HttpContext context) =>
+        await mediator.Send(new PersonGetForCreateRequest(), context.RequestAborted);
+
+    private async Task<PersonViewModel> CreatePerson(PersonCreateViewModel viewModel, IMediator mediator, HttpContext context) =>
+        mediator.Send(new PersonCreateRequest(viewModel), context.RequestAborted);
+
+    private async Task<Guid> DeletePerson(Guid id, IMediator mediator, HttpContext context) =>
+        await mediator.Send(new PersonDeleteRequest(id), context.RequestAborted);
+
+    private async Task<PersonUpdateViewModel> GetPersonForUpdate(Guid id, IMediator mediator, HttpContext context) =>
+        await mediator.Send(new PersonGetForUpdateRequest(id), context.RequestAborted);
+
+    private async Task<PersonViewModel> PutAfterUpdatePerson(PersonUpdateViewModel viewModel, IMediator mediator, HttpContext context) =>
+        await mediator.Send(new PersonUpdateRequest(viewModel), context.RequestAborted);
 }
