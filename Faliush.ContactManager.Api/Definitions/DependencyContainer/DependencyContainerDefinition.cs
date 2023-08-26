28ed68a897694b7e7da241a0bc45f4885e547212
nickfaliush <faliushdev@gmail.com>
2023-08-26 21:10:34 +0300

    add services to dependency container

--- Faliush.ContactManager.Api/Definitions/DependencyContainer/DependencyContainerDefinition.cs
+++ Faliush.ContactManager.Api/Definitions/DependencyContainer/DependencyContainerDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using Faliush.ContactManager.Api.Definitions.Base;
+using Faliush.ContactManager.Core.Services;
 using Faliush.ContactManager.Infrastructure;
 using Faliush.ContactManager.Infrastructure.UnitOfWork;
 
@@ -12,5 +13,9 @@ public class DependencyContainerDefinition : AppDefinition
         builder.Services.AddScoped<IRepositoryFactory, UnitOfWork<ApplicationDbContext>>();
         builder.Services.AddScoped<IUnitOfWork<ApplicationDbContext>, UnitOfWork<ApplicationDbContext>>();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork<ApplicationDbContext>>();
+
+        // Services
+        builder.Services.AddScoped<IDateCalcualtorService, DateCalculatorService>();
+        builder.Services.AddScoped<IStringConvertService, StringConvertService>();
     }
 }
