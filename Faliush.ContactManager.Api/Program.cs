d25dac8f93541a3a88363653fa6c9ce799b9af1b
nickfaliush <faliushdev@gmail.com>
2023-09-06 20:04:55 +0300

    Rewrite project for clean architecture

--- Faliush.ContactManager.Api/Program.cs
+++ Faliush.ContactManager.Api/Program.cs
@@ -1,7 +1,11 @@
 using Faliush.ContactManager.Api.Definitions.Base;
+using Faliush.ContactManager.Core;
+using Faliush.ContactManager.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCoreServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.AddDefinition(typeof(Program));
 
 var app = builder.Build();
