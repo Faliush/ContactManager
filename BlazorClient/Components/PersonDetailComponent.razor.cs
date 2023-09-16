01cef97851c265de9b0fa9d0270c63a5be67c287
nickfaliush <faliushdev@gmail.com>
2023-09-16 18:36:24 +0300

    Create People page and components folder

---
+++ BlazorClient/Components/PersonDetailComponent.razor.cs
@@ -0,0 +1,32 @@
+﻿using BlazorBootstrap;
+using BlazorClient.DTO;
+using BlazorClient.DTO.Results;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorClient.Components;
+
+public class PersonDetailComponentModel : ComponentBase
+{
+    [Inject] HttpClient HttpClient { get; set; }
+    [Inject] NavigationManager NavigationManager { get; set; }
+    protected PersonResult Result { get; set; }
+
+    [Parameter] public Guid PersonId { get; set; }
+    protected Modal? modal = default!;
+
+    protected async Task ShowPersonDetailComponent()
+    {
+        var result = await HttpClient.GetFromJsonAsync<PersonDTO>($"people/{PersonId}");
+
+        if (result is null)
+            NavigationManager.NavigateTo("/error");
+
+        if (!result!.Ok)
+            NavigationManager.NavigateTo($"/error/{result.Exception!.ContainsKey("message")}/{result.Metadata!.ContainsKey("message")}");
+
+        Result = result!.Result;
+
+        await modal.ShowAsync();
+    }
+}
---
+++ BlazorClient/Pages/Error.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient.Pages;
+
+public class ErrorComponent : ComponentBase
+{
+	[Parameter] public string? MetaMessage { get; set; }
+	[Parameter] public string? ExceptionMessage { get; set; }
+
+}
