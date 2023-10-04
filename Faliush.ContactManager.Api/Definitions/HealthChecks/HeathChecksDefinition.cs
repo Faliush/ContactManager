0cacb42b93041c7e9b0e917104ec3a32528495a8
nickfaliush <faliushdev@gmail.com>
2023-10-04 19:12:40 +0300

    add health checks for db in docker

---
+++ Faliush.ContactManager.Api/Definitions/HealthChecks/HeathChecksDefinition.cs
@@ -0,0 +1,18 @@
+﻿using Faliush.ContactManager.Api.Definitions.Base;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Faliush.ContactManager.Api.Definitions.HealthChecks;
+
+public class HeathChecksDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        //builder.Services.AddHealthChecks(); TODO
+    }
+
+    public override void ConfigureApplication(WebApplication app)
+    {
+        //app.MapHealthChecks("/healthz");
+        
+    }
+}
