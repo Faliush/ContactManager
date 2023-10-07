ce66daeee3e1939242eace90e6abfba1c313c4d7
nickfaliush <faliushdev@gmail.com>
2023-10-07 13:56:23 +0300

    Create unit tests for services

---
+++ Faliush.ContactManager.UnitTests/ServiceTests/DateCalculatorServiceTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Faliush.ContactManager.UnitTests.ServiceTests;
+
+public class DateCalculatorServiceTests
+{
+    private readonly IDateCalcualtorService _dateTimeCalculatorService;
+
+    public DateCalculatorServiceTests()
+    {
+        _dateTimeCalculatorService = new DateCalculatorService();
+    }
+
+    [Fact]
+    [Trait("ServiceTests", nameof(DateCalculatorService))]
+    public void DateCalculatorService_ShouldReturnProperAge_WhenDateTimeIsvalid()
+    {
+        var date = DateTime.Parse("08.12.2004");
+        var expected = 18;
+
+        var result =  _dateTimeCalculatorService.GetTotalYears(date);
+
+        result.Should().Be(expected);
+    }
+}
