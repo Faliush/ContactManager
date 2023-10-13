cf46d215026fb157a32f59cd0bda3e1bc31a00c8
nickfaliush <faliushdev@gmail.com>
2023-10-13 14:20:27 +0300

    add serilog to the project

---
+++ Faliush.ContactManager.Api/Definitions/Serilog/SerilogDefinition.cs
@@ -0,0 +1,16 @@
+﻿using Faliush.ContactManager.Api.Definitions.Base;
+using Serilog;
+
+namespace Faliush.ContactManager.Api.Definitions.Serilog;
+
+public class SerilogDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider provider, LoggerConfiguration configuration) =>
+        {
+            configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(provider);
+        });
+    }
+
+}
