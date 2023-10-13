1d2faa04ee0431f2acd1cf76480ef768a918f539
nickfaliush <faliushdev@gmail.com>
2023-10-13 14:21:05 +0300

    add logger to all country requests

--- Faliush.ContactManager.Core/Logic/CountryLogic/Queries/CountryUpdate.cs
+++ Faliush.ContactManager.Core/Logic/CountryLogic/Queries/CountryUpdate.cs
@@ -4,6 +4,7 @@ using Faliush.ContactManager.Core.Logic.CountryLogic.ViewModels;
 using Faliush.ContactManager.Infrastructure.UnitOfWork;
 using Faliush.ContactManager.Models;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Faliush.ContactManager.Core.Logic.CountryLogic.Queries;
 
@@ -12,14 +13,20 @@ public record CountryUpdateRequest(CountryUpdateViewModel Model) : IRequest<Oper
 public class CountryUpdateRequestHandler : IRequestHandler<CountryUpdateRequest, OperationResult<CountryViewModel>>
 {
     private readonly IUnitOfWork _unitOfWork;
-    public CountryUpdateRequestHandler(IUnitOfWork unitOfWork) =>
+    private readonly ILogger<CountryUpdateRequestHandler> _logger;
+
+    public CountryUpdateRequestHandler(IUnitOfWork unitOfWork, ILogger<CountryUpdateRequestHandler> logger)
+    {
         _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
 
     public async Task<OperationResult<CountryViewModel>> Handle(CountryUpdateRequest request, CancellationToken cancellationToken)
     {
         var operation = new OperationResult<CountryViewModel>();
         var repository = _unitOfWork.GetRepository<Country>();
 
+        _logger.LogInformation("CountryUpdateRequestHandler checks name for existance in database");
         var item = await repository.GetFirstOrDefaultAsync
             (
                 predicate: x => x.Name == request.Model.Name && x.Id != request.Model.Id
@@ -27,10 +34,12 @@ public class CountryUpdateRequestHandler : IRequestHandler<CountryUpdateRequest,
 
         if(item is not null)
         {
+            _logger.LogError("Given name already exist in database");
             operation.AddError(new ContactManagerArgumentException($"country with name {request.Model.Name} already exist"));
             return operation;
         }
 
+        _logger.LogInformation("CountryUpdateRequestHandler checks given id for existence in database");
         var entity = await repository.GetFirstOrDefaultAsync
             (
                 predicate: x => x.Id == request.Model.Id
@@ -38,6 +47,7 @@ public class CountryUpdateRequestHandler : IRequestHandler<CountryUpdateRequest,
 
         if (entity is null)
         {
+            _logger.LogError("Country with given id doesn't exist");
             operation.AddError(new ContactManagerNotFoundException($"country with id: {request.Model.Id} not found"));
             return operation;
         }
@@ -46,9 +56,11 @@ public class CountryUpdateRequestHandler : IRequestHandler<CountryUpdateRequest,
 
         repository.Update(entity);
         await _unitOfWork.SaveChangesAsync();
+        _logger.LogInformation("Update given country");
 
         if (!_unitOfWork.LastSaveChangeResult.IsOk)
         {
+            _logger.LogError("Arose exception during data saving");
             var exception = _unitOfWork.LastSaveChangeResult.Exception ?? new ContactManagerSaveDatabaseException();
             operation.AddError(exception);
             return operation;
@@ -61,7 +73,7 @@ public class CountryUpdateRequestHandler : IRequestHandler<CountryUpdateRequest,
         };
 
         operation.Result = result;
-
+        _logger.LogInformation("Country was updated successfully");
         return operation;
     }
 }
