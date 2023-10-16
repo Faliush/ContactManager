d35ec79afdf92e84b40907988ea98b2bcba275f5
nickfaliush <faliushdev@gmail.com>
2023-10-16 20:34:02 +0300

    Connect redis

---
+++ Faliush.ContactManager.Api/Definitions/Redis/RedisDefinition.cs
@@ -0,0 +1,14 @@
+﻿using Faliush.ContactManager.Api.Definitions.Base;
+
+namespace Faliush.ContactManager.Api.Definitions.Redis;
+
+public class RedisDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = builder.Configuration.GetConnectionString("Redis");
+        });
+    }
+}
